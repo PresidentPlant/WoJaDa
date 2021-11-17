1f57c4c4893411684e76426674ac90f35ba738c1
10022795 <10022795@american.fmtusd.org>
2021-11-17 12:35:23 -0800

    Added homepage and registration page 11/17/21

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/homepage')
 def landing_page():
     return render_template("index.html")
+
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    if request.method == "GET":
+        flash("Enter Details to Register")
+        return render_template("register.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        dob = request.form["dob"]
+        password = request.form["password"]
+        if name == "" or email == "" or password == "" or dob == "":
+            flash("Please fill out the missing fields")
+        return redirect("/registration")
+
+# def login():
+
 
 
 if __name__ == "__main__":
